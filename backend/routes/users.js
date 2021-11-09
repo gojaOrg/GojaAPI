f3aafac368f6dd109ddcb1786dbc56b75f365c7d
Oscar Englöf <oscar.englof@hotmail.com>
2021-11-09 13:26:32 +0100

    create user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,4 @@
+var axios = require("axios");
 var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
@@ -6,7 +7,24 @@ const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 const upload = require("../middleware/imageUpload");
-
 const auth = require("../middleware/auth");
+
+router.post(
+  "/",
+  /*auth,*/ async (req, res) => {
+    axios
+      .post(process.env.USERS_SERVICE_URL + "/users", {
+        userName: "erkanerk",
+        email: "erkannn@erk.com",
+        password: "123",
+      })
+      .then(function (response) {
+        res.json(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+);
 
 module.exports = router;
