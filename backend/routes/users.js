b5d04b3dc482adf7022eb8c5e2a573dd9f62c5c4
Oscar Englöf <oscar.englof@hotmail.com>
2021-11-16 10:50:20 +0100

    login user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,19 +8,27 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
-router.post(
-  "/signup",
-  /*auth,*/ async (req, res) => {
-    var form = req.body;
-    axios
-      .post(process.env.USERS_SERVICE_URL + "/users/signup", form)
-      .then(function (response) {
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-);
+router.post("/signup", async (req, res) => {
+  var form = req.body;
+  axios
+    .post(process.env.USERS_SERVICE_URL + "/users/signup", form)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.status(error.response.status).json(error.response.data);
+    });
+});
+router.post("/login", async (req, res) => {
+  var form = req.body;
+  axios
+    .post(process.env.USERS_SERVICE_URL + "/users/login", form)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.status(error.response.status).json(error.response.data);
+    });
+});
 
 module.exports = router;
