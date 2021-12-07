4a9cf52a8127503867932112d2205a0605f04879
Oscar Englöf <oscar.englof@hotmail.com>
2021-12-07 16:27:49 +0100

    userId fix

--- backend/utils/createSearchUserObj.js
+++ backend/utils/createSearchUserObj.js
@@ -2,13 +2,13 @@ const createSearchObj = (isFollowingList, searchResultList) => {
   const returnObjectList = searchResultList.map((user) => {
     const userName = user.userName;
     const profilePicture = user.profilePicture;
-    const _id = user._id;
+    const userId = user._id;
     const isFollowing = isFollowingList.some((u) => {
       return u.userId === user._id;
     });
 
     return {
-      _id,
+      userId,
       userName,
       profilePicture,
       isFollowing,
